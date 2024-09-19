c26b7dbd8756bc784536e7d433ac0327d92f802f
Jack Anderson <whotfisjax@gmail.com>
2024-09-19 16:06:58 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Window, WindowHeader, WindowContent, Button, Toolbar, Panel } from 'react95';
+import { Window, WindowHeader, WindowContent, Button, Toolbar, Panel, MenuListItem, MenuList, Handle } from 'react95';
 import { ThemeProvider } from 'styled-components';
 import original from 'react95/dist/themes/original';
 import Draggable from 'react-draggable';
@@ -18,8 +18,16 @@ const quotes = [
   "Wesa gonna change the game! Remember the VMAs?",
 ];
 
+const planets = {
+  Naboo: 'n.jpg',
+  Coruscant: 'c.jpg',
+  Tatooine: 't.jpg',
+  Geonosis: 'g.jpg',
+};
+
 export default function App() {
   const [randomQuote, setRandomQuote] = useState(quotes[0]);
+  const [selectedPlanet, setSelectedPlanet] = useState('n.jpg'); // Default to Tatooine (t.jpg)
   const audioRef = useRef(new Audio('https://ia601203.us.archive.org/34/items/datpiff-mixtape-m9943056/04%20-%20The%20Good%20Life.mp3'));
   const [isPlaying, setIsPlaying] = useState(false);
 
@@ -36,30 +44,47 @@ export default function App() {
     setIsPlaying(!isPlaying);
   };
 
+  const handlePlanetChange = (planet) => {
+    setSelectedPlanet(planets[planet]);
+  };
+
   return (
     <ThemeProvider theme={original}>
       <div
         className="h-screen w-screen flex flex-col justify-center items-center"
-        style={{ backgroundImage: `url('./bg.jpg')`, backgroundSize: 'cover' }}
+        style={{ backgroundImage: `url('./${selectedPlanet}')`, backgroundSize: 'cover' }}
       >
+        <div className="absolute top-5 left-5">
+          <MenuList inline>
+            <MenuListItem square disabled>
+              <span role="img" aria-label="🌐">🌐</span>
+            </MenuListItem>
+            <Handle size={38} />
+            {Object.keys(planets).map((planet) => (
+              <MenuListItem key={planet} onClick={() => handlePlanetChange(planet)}>
+                {planet}
+              </MenuListItem>
+            ))}
+          </MenuList>
+        </div>
 
-          <div className='absolute top-5 right-5 flex items-center z-[50]'>
-            <a href="https://x.com/" className='transition ease-in-out duration-150'>
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-10 md:size-12 md:hover:scale-105 transition ease-in-out duration-150 cursor-pointer' fill="#000000" viewBox="0 0 50 50">
-                <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
-              </svg>
-            </a>
-            <a href="https://t.me/" className='transition ease-in-out duration-150'>
-              <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-10 md:size-12  md:hover:scale-105 transition ease-in-out duration-150 cursor-pointer' fill="#00aaff" viewBox="0 0 50 50">
-                <path d="M46.137,6.552c-0.75-0.636-1.928-0.727-3.146-0.238l-0.002,0C41.708,6.828,6.728,21.832,5.304,22.445	c-0.259,0.09-2.521,0.934-2.288,2.814c0.208,1.695,2.026,2.397,2.248,2.478l8.893,3.045c0.59,1.964,2.765,9.21,3.246,10.758	c0.3,0.965,0.789,2.233,1.646,2.494c0.752,0.29,1.5,0.025,1.984-0.355l5.437-5.043l8.777,6.845l0.209,0.125	c0.596,0.264,1.167,0.396,1.712,0.396c0.421,0,0.825-0.079,1.211-0.237c1.315-0.54,1.841-1.793,1.896-1.935l6.556-34.077	C47.231,7.933,46.675,7.007,46.137,6.552z M22,32l-3,8l-3-10l23-17L22,32z"></path>
-              </svg>
-            </a>
-          </div>
+        <div className="absolute bottom-5 right-5 flex items-center z-[50]">
+          <a href="https://x.com/" className="transition ease-in-out duration-150">
+            <svg xmlns="http://www.w3.org/2000/svg" className="size-10 md:size-12 md:hover:scale-105 transition ease-in-out duration-150 cursor-pointer" fill="#000000" viewBox="0 0 50 50">
+              <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
+            </svg>
+          </a>
+          <a href="https://t.me/" className="transition ease-in-out duration-150">
+            <svg xmlns="http://www.w3.org/2000/svg" className="size-10 md:size-12  md:hover:scale-105 transition ease-in-out duration-150 cursor-pointer" fill="#00aaff" viewBox="0 0 50 50">
+              <path d="M46.137,6.552c-0.75-0.636-1.928-0.727-3.146-0.238l-0.002,0C41.708,6.828,6.728,21.832,5.304,22.445 c-0.259,0.09-2.521,0.934-2.288,2.814c0.208,1.695,2.026,2.397,2.248,2.478l8.893,3.045c0.59,1.964,2.765,9.21,3.246,10.758 c0.3,0.965,0.789,2.233,1.646,2.494c0.752,0.29,1.5,0.025,1.984-0.355l5.437-5.043l8.777,6.845l0.209,0.125 c0.596,0.264,1.167,0.396,1.712,0.396c0.421,0,0.825-0.079,1.211-0.237c1.315-0.54,1.841-1.793,1.896-1.935l6.556-34.077 C47.231,7.933,46.675,7.007,46.137,6.552z M22,32l-3,8l-3-10l23-17L22,32z"></path>
+            </svg>
+          </a>
+        </div>
 
         <Draggable>
           <Window className="w-80">
             <WindowHeader className="flex justify-between items-center">
-              <span>Yeye Binks v1.0</span>
+              <span>YeYe Binks v1.0</span>
               <Button size="sm" square>
                 <span className="text-lg">×</span>
               </Button>
